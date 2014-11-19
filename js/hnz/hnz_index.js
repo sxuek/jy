b19ab907ef2bf918dbd35beb89a6fb85ceeac8d6
HouNingZhou <allcky@qq.com>
2014-11-19 19:04:33 +0800

    change index.js serach btn bgimg

--- js/hnz/hnz_index.js
+++ js/hnz/hnz_index.js
@@ -31,8 +31,8 @@ $(function(){
 	//login btns click
 	$('.hnz_logo-box .hnz_btn').mousedown(function(){$(this).css({background:'#006c6c'})});
 	$('.hnz_logo-box .hnz_btn').mouseup(function(){$(this).css({background:'#009494'})});
-	$('.hnz_logo-box .hnz_search-btn').mousedown(function(){$(this).css({background:'#009494 url(../images/hnz_search_click.gif) no-repeat 0 0'})});
-	$('.hnz_logo-box .hnz_search-btn').mouseup(function(){$(this).css({background:'#009494 url(../images/hnz_search.gif) no-repeat 0 0'})});
+	$('.hnz_logo-box .hnz_search-btn').mousedown(function(){$(this).css({background:'#009494 url(images/hnz_search_click.gif) no-repeat 0 0'})});
+	$('.hnz_logo-box .hnz_search-btn').mouseup(function(){$(this).css({background:'#009494 url(images/hnz_search.gif) no-repeat 0 0'})});
 	//nav
 	var t;
 	$('.hnz_nav li:not(:first)').hover(function(){
