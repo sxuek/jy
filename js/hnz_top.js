d71a522f2556c54a048df43a024d30665aa151ae
HouNingZhou <allcky@qq.com>
2014-11-20 00:37:50 +0800

    change nav js bug

--- js/hnz_top.js
+++ js/hnz_top.js
@@ -6,16 +6,17 @@ $(function(){
 	$('.hnz_logo-box .hnz_search-btn').mouseup(function(){$(this).css({background:'#009494 url(../images/hnz_search.gif) no-repeat 0 0'})});
 	//nav
 	var t;
+	$('.hnz_nav li>a').hover(function(){$(this).css({background:'#009494',color:'#fff'});},function(){
+		$(this).css({background:'',color:''});
+	})
 	$('.hnz_nav li:not(:first)').hover(function(){
 		var index=$('.hnz_nav li:not(:first)').index(this);
-		$('.hnz_nav li:not(:first)>a:eq('+index+')').css({background:'#009494',color:'#fff'});
 		$(this).find('i').css({background:'url(../images/hnz_arrow_hot.png) no-repeat 0 0'})
 		t=setTimeout(function(){
-			$('.hnz_nav li:not(:first) dl:eq('+index+')').slideDown(300);
+			$('.hnz_nav li dl:eq('+index+')').slideDown(300);
 			},150)
 		},function(){
-			$('.hnz_nav li:not(:first)>a').css({background:'',color:''});
-			$(this).find('i').css({background:'url(../images/hnz_arrow.png) no-repeat 0 0'});
+			$('.hnz_nav li:not("#hnz_hot") i').css({background:'url(../images/hnz_arrow.png) no-repeat 0 0'});
 			clearTimeout(t);
 			$('.hnz_nav li dl').slideUp(200);
 	})
