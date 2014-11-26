cbdcf9fa08faf9028ea64aae3c749e6d3c05491b
HouNingZhou <allcky@qq.com>
2014-11-26 10:02:40 +0800

    修改 首页  模版头部 js 引用背景图片问题

--- js/hnz/hnz_index.js
+++ js/hnz/hnz_index.js
@@ -33,8 +33,14 @@ $(function(){
 	//login btns click
 	$('.hnz_logo-box .hnz_btn').mousedown(function(){$(this).css({background:'#006c6c'})});
 	$('.hnz_logo-box .hnz_btn').mouseup(function(){$(this).css({background:'#009494'})});
-	$('.hnz_logo-box .hnz_search-btn').mousedown(function(){$(this).css({background:'#009494 url(images/hnz_search_click.gif) no-repeat 0 0'})});
-	$('.hnz_logo-box .hnz_search-btn').mouseup(function(){$(this).css({background:'#009494 url(images/hnz_search.gif) no-repeat 0 0'})});
+	$('.hnz_logo-box .hnz_search-btn').mousedown(function(){
+		$(this).addClass('hnz_search-btn-down');
+		$(this).removeClass('hnz_search-btn-up');
+		});
+	$('.hnz_logo-box .hnz_search-btn').mouseup(function(){
+		$(this).removeClass('hnz_search-btn-down');
+		$(this).addClass('hnz_search-btn-up');
+		});
 	//nav
 	var t;
 	$('.hnz_nav li>a').hover(function(){$(this).css({background:'#009494',color:'#fff'});},function(){
@@ -42,12 +48,12 @@ $(function(){
 	})
 	$('.hnz_nav li:not(:first)').hover(function(){
 		var index=$('.hnz_nav li:not(:first)').index(this);
-		$(this).find('i').css({background:'url(images/hnz_arrow_hot.png) no-repeat 0 0'})
+		$(this).find('i').addClass('hnz_nav-i');
 		t=setTimeout(function(){
 			$('.hnz_nav li dl:eq('+index+')').slideDown(300);
 			},150)
 		},function(){
-			$('.hnz_nav li:not("#hnz_hot") i').css({background:'url(images/hnz_arrow.png) no-repeat 0 0'});
+			$('.hnz_nav li:not("#hnz_hot") i').removeClass('hnz_nav-i');
 			clearTimeout(t);
 			$('.hnz_nav li dl').slideUp(200);
 	})
