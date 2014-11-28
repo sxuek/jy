18dcc4310f7e67bac3e27cdefd19c9606faa1c94
HouNingZhou <allcky@qq.com>
2014-11-28 09:57:24 +0800

    修复 德秀书香 列表 js效果   布局中多一对UL

--- js/hnz/hnz_dxsx-list.js
+++ js/hnz/hnz_dxsx-list.js
@@ -4,10 +4,11 @@ $(function(){
 	hnz_dxsxImg.hover(function(){
 		var aa=$(this);
 		hnz_dxsxT= setTimeout(function(){
-		aa.animate({width:213,height:281})
-		},200)
+		aa.animate({width:213,height:281},200)
+		},150)
 		},function(){
 			clearInterval(hnz_dxsxT);	
-			hnz_dxsxImg.animate({width:201,height:269})
+			hnz_dxsxImg.stop();
+			hnz_dxsxImg.animate({width:201,height:269},100)
 			})
 	})
