dd10e00e4efbf15b4360ed5647c0fbeee3876813
HouNingZhou <allcky@qq.com>
2014-11-28 13:33:03 +0800

    修改 德秀书香  js

--- js/hnz/hnz_dxsx-list.js
+++ js/hnz/hnz_dxsx-list.js
@@ -3,12 +3,10 @@ $(function(){
 	var hnz_dxsxT;
 	hnz_dxsxImg.hover(function(){
 		var aa=$(this);
-		hnz_dxsxT= setTimeout(function(){
-		aa.animate({width:213,height:281},200)
-		},150)
+		aa.animate({width:213,height:281},200)	
 		},function(){
-			clearInterval(hnz_dxsxT);	
+			var aa=$(this);
 			hnz_dxsxImg.stop();
-			hnz_dxsxImg.animate({width:201,height:269},100)
+			aa.animate({width:201,height:269},100)
 			})
 	})
