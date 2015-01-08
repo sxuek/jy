07a7f0b23d983b5364806dd2f610b9d097d5173a
ma yanlong <l.luffy.river@gmail.com>
2015-01-09 00:36:37 +0800

    山大往事最终版
    小删减和改动

--- js/myl/new-myl-sdws-list.js
+++ js/myl/new-myl-sdws-list.js
@@ -2,17 +2,22 @@ $(function(){
 
     $(".sdws-list-box dd").hide();
     $(".sdws-list-box dl:first").find('dd').show();
-    $(".plus-button:first").find("img").attr("src","../images/myl/new-myl-sdws-jianhao.png");
-    $('.plus-button:first').addClass('zhankai');
 
-    $(".plus-button").click(function(){
-        $(this).parent().parent().find("dd").toggle();
+    $(".plus-button:first").find("img").attr("src","../images/myl/new-myl-sdws-jianhao.png");
+    $('.sdws-item dt:first').addClass('zhankai');
+
+    $('.sdws-item').each(function(){
+        $(this).find('dd:last').css('border','none');
+    })
+
+    $(".sdws-item dt").click(function(){
+        $(this).parent().find("dd").toggle();
         $(this).toggleClass('zhankai');
 
         if($(this).hasClass('zhankai')){
-            $(this).find('img').attr("src","../images/myl/new-myl-sdws-jianhao.png");
+            $(this).find('.plus-button img').attr("src","../images/myl/new-myl-sdws-jianhao.png");
         }else{
-            $(this).find('img').attr("src","../images/myl/new-myl-sdws-jiahao.png");
+            $(this).find('.plus-button img').attr("src","../images/myl/new-myl-sdws-jiahao.png");
         }
     });
 });
